62d4c66eaed3fbce60b257c8a3d4ed7219f20ae1
aymaneElyamani <aymaneelyamani30@gmail.com>
2025-11-23 13:54:05 +0000

    Initialize Spring MVC application with Product entity, repository, and basic configuration

---
+++ src/main/java/ma/enset/bdccensetspringmvc/BdccEnsetSpringMvcApplication.java
@@ -0,0 +1,55 @@
+package ma.enset.bdccensetspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import ma.enset.bdccensetspringmvc.entities.Product;
+import ma.enset.bdccensetspringmvc.repository.ProductRepository;
+
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+public class BdccEnsetSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdccEnsetSpringMvcApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner
+            (ProductRepository productRepository) {
+        return args -> {
+            productRepository.save(
+                    Product.builder()
+                            .name("Computer")
+                            .price(5400)
+                            .quantity(23)
+                            .build()
+            );
+            productRepository.save(
+                    Product.builder()
+                            .name("Printer")
+                            .price(1200)
+                            .quantity(22)
+                            .build()
+            );
+            productRepository.save(
+                    Product.builder()
+                            .name("Smart Phone")
+                            .price(1200)
+                            .quantity(10)
+                            .build()
+            );
+            productRepository.findAll()
+                    .forEach(p->{
+                        System.out.println(p.toString());
+                    });
+        };
+    }
+
+
+}
---
+++ src/main/java/ma/enset/bdccensetspringmvc/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package ma.enset.bdccensetspringmvc.repository;
+
+import ma.enset.bdccensetspringmvc.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository
+        extends JpaRepository<Product, Long> {
+}
