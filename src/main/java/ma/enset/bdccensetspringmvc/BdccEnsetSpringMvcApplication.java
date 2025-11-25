cc8168b8b8f3eba9b05c88107e816623c2797459
aymaneElyamani <aymaneelyamani30@gmail.com>
2025-11-25 23:31:28 +0000

    simplify SpringBootApplication annotation

--- src/main/java/ma/enset/bdccensetspringmvc/BdccEnsetSpringMvcApplication.java
+++ src/main/java/ma/enset/bdccensetspringmvc/BdccEnsetSpringMvcApplication.java
@@ -1,18 +1,14 @@
 package ma.enset.bdccensetspringmvc;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import ma.enset.bdccensetspringmvc.entities.Product;
 import ma.enset.bdccensetspringmvc.repository.ProductRepository;
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class BdccEnsetSpringMvcApplication {
 
     public static void main(String[] args) {
